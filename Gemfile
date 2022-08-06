6852fc2f73bfc510553adc0bce7952f42428359b
Nathan Smith <nathan@nathansmith.io>
2022-08-06 11:25:11 -0700

    Use plist instead

--- Gemfile
+++ Gemfile
@@ -4,4 +4,4 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'gemoji', github: 'github/gemoji'
-gem 'sqlite3'
+gem 'plist'
--- main.rb
+++ main.rb
@@ -1,28 +1,16 @@
 require 'gemoji'
-require "sqlite3"
+require 'plist'
 
-user = ENV['USER']
-paths = Dir.glob("/Users/#{user}/Library/Dictionaries/CoreDataUbiquitySupport/#{user}*/UserDictionary/local/store/UserDictionary.db")
+snippets = []
 
-return if paths.nil? or paths.length != 1
-
-db = SQLite3::Database.new(paths.first)
-db.execute("DELETE FROM 'ZUSERDICTIONARYENTRY';")
-
-id = 1
 Emoji.all.each do |emoji|
   next if emoji.custom?
   emoji.aliases.each do |name|
-    snippet = ":#{name.gsub('_', ' ')}:"
-    puts "Adding #{emoji.raw} as #{snippet}..."
-    item = "{on=1;replace=\"#{snippet}\";with=\"#{emoji.raw}\";}"
-    system("defaults write -g NSUserDictionaryReplacementItems -array-add '#{item}'")
-    db.execute("INSERT INTO 'ZUSERDICTIONARYENTRY' VALUES(?, 1,1,0,0,0,0, ?,NULL,NULL,NULL,NULL,NULL, ?, ?,NULL);", [id, Time.now.to_i, snippet, emoji.raw])
-    id += 1
+    snippet = ":#{name}:"
+    snippets.push({shortcut: snippet, phrase: emoji.raw})
   end
 end
 
-puts "Loading changes..."
-system("defaults read -g NSUserDictionaryReplacementItems")
-system("killall cfprefsd")
-puts "Done!"
+File.open("emoji.plist", "w") do |f|
+  f.write(snippets.to_plist)
+end
